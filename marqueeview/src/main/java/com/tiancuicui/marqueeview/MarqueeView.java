925084901818c979c780f64877fe75dab354a090
like <like@imooc.com>
2017-08-16 18:41:46 +0800

    添加修改文字色彩的方法

--- marqueeview/src/main/java/com/tiancuicui/marqueeview/MarqueeView.java
+++ marqueeview/src/main/java/com/tiancuicui/marqueeview/MarqueeView.java
@@ -131,6 +131,18 @@ public class MarqueeView<T> extends ViewFlipper {
         return tv;
     }
 
+    // 设置文字色彩
+    public void setMarqueeTextColor(int color) {
+        textColor = color;
+        final int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child instanceof TextView) {
+                ((TextView)child).setTextColor(color);
+            }
+        }
+    }
+
     public int getPosition() {
         return (int) getCurrentView().getTag();
     }
